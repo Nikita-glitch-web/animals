010d0f23722b7f8969fc526c3da27aba5e042fae
Nick <nikitalytvynov0506@gmail.com>
2023-08-24 16:58:34 +0300

    si

---
+++ src/AnimalShow.js
@@ -0,0 +1,12 @@
+function AnimalShow() {
+    // const handleClick = () => {
+    //     console.log('button was clicked');
+    // }
+    return <div>
+        <button onClick={() => console.log('Button was clicked')}>
+            Add Animal
+        </button>
+    </div>
+}
+
+export default AnimalShow;
